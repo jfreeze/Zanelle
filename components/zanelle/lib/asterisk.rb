ec05127c165f1cee3c7cfd2b1ad9fb8c8e55334e
Jim Freeze <github@freeze.org>
2010-12-06 09:37:05 -0600

    rearranging for dialplan directory

--- components/zanelle/lib/asterisk.rb
+++
@@ -1,7 +0,0 @@
-if `which asterisk`.empty?  
-  require 'asterisk_mock'
-else
-  require 'asterisk_api'
-end
-
-
--- components/zanelle/lib/zanelle/ring_set.rb
+++
@@ -1,8 +0,0 @@
-module Zanelle
-  class RingSet < Loader
-    attr_reader :extensions, :duration
-    def initialize(input)
-      @data[extensions].collect { |ext_chtype| }
-    end
-  end#class RingSet
-end#module Zanelle
